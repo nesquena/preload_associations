7da1dffd0d13a26ead1a8601092a19fd3e7ccf76
Nathan Esquenazi <nesquena@gmail.com>
2010-11-08 22:12:39 -0800

    Adds support for instance level preload functionality

--- init.rb
+++ init.rb
@@ -2,6 +2,6 @@ require 'active_record_ext'
 require 'array_ext'
 
 # extend activerecord with the :preload method
-ActiveRecord::Base.send(:extend, AssociationsPreload::Base)
+ActiveRecord::Base.send(:include, AssociationsPreload::Base)
 # add :preload method to array
 Array.send(:include, AssociationsPreload::Array)
--- lib/active_record_ext.rb
+++ lib/active_record_ext.rb
@@ -1,5 +1,18 @@
 module AssociationsPreload
   module Base
+    def self.included(base)
+      base.send(:include, AssociationsPreload::InstanceMethods)
+      base.send(:extend, AssociationsPreload::ClassMethods)
+    end
+  end
+
+  module InstanceMethods
+    def preload(*args)
+      self.class.preload(self, *args)
+    end
+  end
+
+  module ClassMethods
     def preload(records, *args)
       include_associations = send(:merge_includes, send(:scope, :find, :include), args)
       preload_associations(records, include_associations) unless include_associations.empty?
